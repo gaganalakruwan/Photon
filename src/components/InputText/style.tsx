6933f3c2bc1d3acd6f609f282b5fb06ae80ce8b1
ImalMS <imalmenukas210@gmail.com>
2024-07-16 11:29:14 +0530

    inputtext

---
+++ src/components/InputText/style.tsx
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 5,
+  },
+  underline: {
+    height: 1,
+    backgroundColor: '#333',
+    marginTop: 5,
+  },
+});
+
+export default styles;
