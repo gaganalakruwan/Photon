a6c7a1388efaa174272b06833b7acb0d1a0fb64c
it21119026 <it21119026@my.sliit.lk>
2024-07-22 10:14:54 +0530

    Start Screens

---
+++ src/components/ShowHidden/style.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#F5EEDC",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingHorizontal: 12,
+    },
+    inputContainer: {
+      backgroundColor: "white",
+      width: "100%",
+      borderRadius: 8,
+      flexDirection: "row",
+      alignItems: "center",
+      borderWidth: 4,
+      borderColor: "#d7d7d7",
+    },
+    inputField: {
+      padding: 14,
+      fontSize: 22,
+      width: "90%",
+    },
+  });
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react';
+import InputText from '../../components/InputText/InputText';
+
+import {Alert, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StackScreenProps} from '@react-navigation/stack';
+import {StackParameterList} from '../../navigation/type';
+import ActionButton from '../../components/ActionButton/ActionButton';
+import {useNavigation} from '@react-navigation/native';
+import navigation from '../../navigation/navigation';
 
 const Home = () => {
   return (
     <View>
-      <Text style={{color:'black'}}>Home</Text>
+      <Text style={{color:'black'}}>Home ME </Text>
+      {/* <ActionButton
+          title={'Login'}
+          onPress={() => navigation.navigate('SIGNUP' as never)}
+          containerStyle={{alignSelf: 'center',marginTop:50}}
+        /> */}
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
